596d4d7fa47008b196ad10b54499be277795f384
VitthalGund <vitthalgund30@gmail.com>
2023-11-27 22:30:55 +0530

    final optimization and bug fixes

--- text-to-voice/src/components/TextToVoice.tsx
+++ text-to-voice/src/components/TextToVoice.tsx
@@ -1,86 +1,58 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect, useMemo } from "react";
 
 const TextToVoice = () => {
-
-    const [rate, setRate] = useState(0);
-    const [rateLabel, setRateLabel] = useState(0);
-    const [volume, setVolume] = useState(0);
-    const [volumeLabel, setVolumeLabel] = useState(0);
-    const [pitch, setPitch] = useState(0);
-    const [pitchLabel, setPitchLabel] = useState(0);
-
+    const [rate, setRate] = useState(1);
+    const [volume, setVolume] = useState(1);
+    const [pitch, setPitch] = useState(1);
+    const [selectedVoiceIndex, setSelectedVoiceIndex] = useState(0);
+    const [textInput, setTextInput] = useState("Hey its vitthal");
+    const [voices, setVoices] = useState<SpeechSynthesisVoice[]>([]);
 
     const start = useRef<HTMLButtonElement>(null);
     const pause = useRef<HTMLButtonElement>(null);
     const resume = useRef<HTMLButtonElement>(null);
     const cancel = useRef<HTMLButtonElement>(null);
     const voicesList = useRef<HTMLSelectElement>(null);
-    const [textInput, setTextInput] = useState("");
 
-    // Initialize new SpeechSynthesisUtterance object
-    const speech = new SpeechSynthesisUtterance();
-
-    // Set Speech Language
+    const speech = useMemo(() => new SpeechSynthesisUtterance(), []);
+    // const speech = new SpeechSynthesisUtterance();
     speech.lang = "en";
 
-    let voices: SpeechSynthesisVoice[] = window.speechSynthesis.getVoices(); // global array of available voices
+    useEffect(() => {
+        const handleVoicesChanged = () => {
+            const availableVoices = window.speechSynthesis.getVoices();
+            setVoices(availableVoices);
+            setSelectedVoiceIndex(0);
+        };
 
-    window.speechSynthesis.onvoiceschanged = () => {
-        // Get List of Voices
-        voices = window.speechSynthesis.getVoices();
-        // console.log(voices)
-        // Initially set the First Voice in the Array.
-        speech.voice = voices[0];
+        window.speechSynthesis.onvoiceschanged = handleVoicesChanged;
 
-        // Set the Voice Select List. (Set the Index as the value, which we'll use later when the user updates the Voice using the Select Menu.)
+        return () => {
+            window.speechSynthesis.onvoiceschanged = null;
+        };
+    }, []);
+
+    useEffect(() => {
         if (voicesList.current) {
-            voices.forEach((voice: SpeechSynthesisVoice, i: number) => {
+            voicesList.current.options.length = 0;
+            voices.forEach((voice, i) => {
                 if (voicesList.current) {
-                    voicesList.current.options[i] = new Option(voice.name, i + "", true, true)
+                    voicesList.current.options[i] = new Option(voice.name, i.toString());
                 }
-            });
+            })
         }
+        // Update the properties of the existing speech object
+        speech.voice = voices[selectedVoiceIndex];
+        speech.volume = volume;
+        speech.rate = rate;
+        speech.pitch = pitch;
+        speech.text = textInput;
+    }, [voices, selectedVoiceIndex, volume, rate, pitch, textInput, speech]);
+
+    const handleVoiceChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const selectedIndex = parseInt(e.target.value, 10);
+        setSelectedVoiceIndex(selectedIndex);
     };
-
-    if (rate.current) {
-        rate.current.addEventListener("input", () => {
-            // Get rate Value from the input
-            // Set rate property of the SpeechSynthesisUtterance instance
-            speech.rate = parseInt(rate.current?.value as string);
-
-            // Update the rate label
-            if (rateLabel.current) {
-                rateLabel.current.innerHTML = rate.current?.value as string;
-            }
-        });
-    }
-
-    if (volume.current) {
-        volume.current.addEventListener("input", () => {
-            // Get volume Value from the input
-            // Set volume property of the SpeechSynthesisUtterance instance
-            speech.volume = parseInt(volume.current?.value as string);
-
-            // Update the volume label
-            if (volumeLabel.current) {
-                volumeLabel.current.innerHTML = volume.current?.value as string;
-            }
-        });
-    }
-
-    if (pitch.current) {
-        pitch.current.addEventListener("input", () => {
-            // Get pitch Value from the input
-            // Set pitch property of the SpeechSynthesisUtterance instance
-            speech.pitch = parseInt(pitch.current?.value as string);
-
-            // Update the pitch label
-            if (pitchLabel.current) {
-                pitchLabel.current.innerHTML = pitch.current?.value as string;
-            }
-        });
-    }
-
 
     return (
         <>
@@ -91,9 +63,8 @@ const TextToVoice = () => {
                 <select
                     ref={voicesList}
                     id="voicesList"
-                    onChange={(e) => {
-                        speech.voice = voices[e.currentTarget.selectedIndex];
-                    }}
+                    onChange={handleVoiceChange}
+                    value={selectedVoiceIndex}
                     className="form-select bg-secondary text-light"
                 ></select>
 
@@ -101,7 +72,8 @@ const TextToVoice = () => {
                     <div className="m-auto">
                         <p className="lead">Volume</p>
                         <input value={volume} type="range" min={0} max={10} step={0.1} id="volume" onChange={(e) => {
-                            speech.volume = volume;
+                            console.log(parseFloat(e.target.value))
+                            speech.volume = (parseFloat(e.target.value));
                             setVolume(e.target.valueAsNumber);
                         }} />
                         <span id="volume-label" className="ms-2">{volume}</span>
@@ -109,7 +81,7 @@ const TextToVoice = () => {
                     <div className="m-auto">
                         <p className="lead">Rate</p>
                         <input value={rate} type="range" min={0} max={10} onChange={(e) => {
-                            speech.rate = rate;
+                            speech.rate = e.target.valueAsNumber;
                             setRate(e.target.valueAsNumber);
                         }} id="rate" step={0.1} />
                         <span id="rate-label" className="ms-2">{rate}</span>
@@ -117,7 +89,7 @@ const TextToVoice = () => {
                     <div className="m-auto">
                         <p className="lead">Pitch</p>
                         <input value={pitch} type="range" min={0} max={10} onChange={(e) => {
-                            speech.pitch = pitch;
+                            speech.pitch = e.target.valueAsNumber;
                             setPitch(e.target.valueAsNumber);
                         }} step={0.1} id="pitch" />
                         <span id="pitch-label" className="ms-2">{pitch}</span>
@@ -127,28 +99,39 @@ const TextToVoice = () => {
                 <textarea value={textInput} onChange={(e) => setTextInput(e.target.value)} className="form-control bg-dark text-light mt-5" cols={30} rows={10} placeholder="Type here..."></textarea>
 
                 <div className="mb-5 d-flex flex-wrap justify-content-center align-content-center">
-                    <button ref={start} id="start" className="btn btn-success mt-5 me-3"
+                    <button
+                        ref={start} id="start"
+                        className="btn btn-success mt-5 me-3"
                         onClick={() => {
                             // Set the text property with the value of the textarea
                             speech.text = textInput;
                             // Start Speaking
+                            speech.volume = volume;
+                            // console.log(speech)
                             window.speechSynthesis.speak(speech);
                         }}
                     >Start</button>
-                    <button ref={pause}
+                    <button
+                        ref={pause}
                         id="pause"
                         className="btn btn-warning mt-5 me-3"
                         onClick={() => {
                             window.speechSynthesis.pause();
                         }}
                     >Pause</button>
-                    <button ref={resume} id="resume" className="btn btn-info mt-5 me-3"
+                    <button
+                        ref={resume}
+                        id="resume"
+                        className="btn btn-info mt-5 me-3"
                         onClick={() => {
                             // Resume the paused speechSynthesis instance
                             window.speechSynthesis.resume();
                         }}
                     >Resume</button>
-                    <button ref={cancel} id="cancel" className="btn btn-danger mt-5 me-3"
+                    <button
+                        ref={cancel}
+                        id="cancel"
+                        className="btn btn-danger mt-5 me-3"
                         onClick={() => {
                             // Cancel the speechSynthesis instance
                             window.speechSynthesis.cancel();
